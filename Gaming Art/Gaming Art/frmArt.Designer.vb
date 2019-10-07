7d95ed7c8f339690b99d0dc286c4a43c3bd06958
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-07 09:51:46 -0500

    created picArt

--- Gaming Art/Gaming Art/frmArt.Designer.vb
+++ Gaming Art/Gaming Art/frmArt.Designer.vb
@@ -22,17 +22,33 @@ Partial Class frmArt
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.picArt = New System.Windows.Forms.PictureBox()
+        CType(Me.picArt, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'picArt
+        '
+        Me.picArt.Image = Global.Gaming_Art.My.Resources.Resources.art
+        Me.picArt.Location = New System.Drawing.Point(24, 23)
+        Me.picArt.Name = "picArt"
+        Me.picArt.Size = New System.Drawing.Size(293, 160)
+        Me.picArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picArt.TabIndex = 0
+        Me.picArt.TabStop = False
         '
         'frmArt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(654, 411)
+        Me.Controls.Add(Me.picArt)
         Me.Name = "frmArt"
         Me.Text = "Gaming Art Commission - Game Art & Design Competition"
+        CType(Me.picArt, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents picArt As PictureBox
 End Class
