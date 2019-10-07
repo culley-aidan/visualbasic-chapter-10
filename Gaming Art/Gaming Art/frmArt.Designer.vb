d4b815d94a9a86a6187081e642d0dbb7782c4ca6
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-07 09:53:23 -0500

    created lblTitle

--- Gaming Art/Gaming Art/frmArt.Designer.vb
+++ Gaming Art/Gaming Art/frmArt.Designer.vb
@@ -23,6 +23,7 @@ Partial Class frmArt
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.picArt = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
         CType(Me.picArt, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -36,19 +37,34 @@ Partial Class frmArt
         Me.picArt.TabIndex = 0
         Me.picArt.TabStop = False
         '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Script MT Bold", 39.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.Green
+        Me.lblTitle.Location = New System.Drawing.Point(339, 45)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(303, 126)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Game Design" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Finalists" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
         'frmArt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(654, 411)
+        Me.Controls.Add(Me.lblTitle)
         Me.Controls.Add(Me.picArt)
         Me.Name = "frmArt"
         Me.Text = "Gaming Art Commission - Game Art & Design Competition"
         CType(Me.picArt, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents picArt As PictureBox
+    Friend WithEvents lblTitle As Label
 End Class
