49c905bb1c8357c0d929c449bd8952350bc754b0
culley-aidan <culley-aidan@users.noreply.github.com>
2019-10-08 09:29:08 -0500

    dab

--- Gaming Art/Gaming Art/frmArt.vb
+++ Gaming Art/Gaming Art/frmArt.vb
@@ -1,3 +1,14 @@
 ﻿Public Class frmArt
+    Private Sub ArtistBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles ArtistBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ArtistBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.ArtDataSet)
 
+    End Sub
+
+    Private Sub FrmArt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'ArtDataSet.Artist' table. You can move, or remove it, as needed.
+        Me.ArtistTableAdapter.Fill(Me.ArtDataSet.Artist)
+
+    End Sub
 End Class
